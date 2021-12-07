73e231403d5e4f2ccc8371bb0e001e721f61e924
UzairAbdullah <uzairmir2703@gmail.com>
2021-12-07 19:01:45 +0500

    VxMutation

--- lib/widgets/home_widgets/add_to_cart.dart
+++ lib/widgets/home_widgets/add_to_cart.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:install_check/core/store.dart';
 import 'package:install_check/models/cart_model.dart';
 import 'package:install_check/models/catalog.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class AddToCart extends StatelessWidget {
   final Item catalog;
@@ -10,19 +12,16 @@ class AddToCart extends StatelessWidget {
     required this.catalog,
   }) : super(key: key);
 
-  final _cart = CartModel();
-
   @override
   Widget build(BuildContext context) {
+    VxState.watch(context, on: [AddMutation]);
+    final CartModel _cart = (VxState.store as MyStore).cart;
+
     bool isInCart = _cart.items.contains(catalog) ?? false;
     return ElevatedButton(
         onPressed: () {
           if (isInCart == false) {
-            isInCart = !isInCart;
-            final _catalog = CatalogModel();
-            _cart.catalog = _catalog;
-            _cart.add(catalog);
-            // setState(() {});
+            AddMutation(catalog);
           }
         },
         child:
